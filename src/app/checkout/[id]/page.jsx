b8afa7077be82d736055b9df7b659dbb9a0bc2db
goodbyeO0 <zikrizhan@gmail.com>
2023-12-13 19:02:24 +0800

    test merged array

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,5 +1,9 @@
-export default async function Checkout({ params }) {
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Checkout({ params }) {
   console.log(params.id);
+  // make an array that has array of id(s) with same id from the params
   const mergedArray = params.id
     .split("a")
     .reduce((acc, value) => {
@@ -17,10 +21,44 @@ export default async function Checkout({ params }) {
       return acc;
     }, []);
   console.log(mergedArray);
+
+  const [product, setProduct] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+      );
+      const json = await res.json();
+      setProduct(json);
+      setIsPending(false);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(product);
+  for (let i = 0; i < product.data.length; i++) {
+    for (let j = 0; j < mergedArray.length; j++) {
+      if (product.data[i] === mergedArray[0][j]) {
+        console.log(product.data[i]);
+      }
+    }
+  }
   return (
     <>
       <h1 className="text-6xl font-semibold">Checkout</h1>
       <div>Delivery</div>
+      {product && product.data.map((dataa, i) => console.log(dataa.title))}
+      {product && product.data.map((dataa, i) => console.log(dataa.mal_id))}
+      {product &&
+        product.data.map((dataa, i) => {
+          mergedArray[0][0] === dataa.mal_id ? (
+            <p>{dataa.title}</p>
+          ) : (
+            <p>Something wrong</p>
+          );
+        })}
     </>
   );
 }
